e588326bbbed1627f810c08a536aa4ca6f533666
Nachasic <nachasic@gmail.com>
2019-03-24 18:48:56 +0200

    Enable all tests

--- src/LinkedList/index.test.ts
+++ src/LinkedList/index.test.ts
@@ -219,29 +219,29 @@ describe('LinkedList', () => {
     expect(linkedList.toString()).toBe('1,1,2,3,3,3,4,5');
   });
 
-//   it('should reverse linked list', () => {
-//     const linkedList = new LinkedList();
+  it('should reverse linked list', () => {
+    const linkedList = new LinkedList();
 
-//     // Add test values to linked list.
-//     linkedList
-//       .append(1)
-//       .append(2)
-//       .append(3);
+    // Add test values to linked list.
+    linkedList
+      .append(1)
+      .append(2)
+      .append(3);
 
-//     expect(linkedList.toString()).toBe('1,2,3');
-//     expect(linkedList.head.value).toBe(1);
-//     expect(linkedList.tail.value).toBe(3);
+    expect(linkedList.toString()).toBe('1,2,3');
+    expect(linkedList.head!.value).toBe(1);
+    expect(linkedList.tail!.value).toBe(3);
 
-//     // Reverse linked list.
-//     linkedList.reverse();
-//     expect(linkedList.toString()).toBe('3,2,1');
-//     expect(linkedList.head.value).toBe(3);
-//     expect(linkedList.tail.value).toBe(1);
+    // Reverse linked list.
+    linkedList.reverse();
+    expect(linkedList.toString()).toBe('3,2,1');
+    expect(linkedList.head!.value).toBe(3);
+    expect(linkedList.tail!.value).toBe(1);
 
-//     // Reverse linked list back to initial state.
-//     linkedList.reverse();
-//     expect(linkedList.toString()).toBe('1,2,3');
-//     expect(linkedList.head.value).toBe(1);
-//     expect(linkedList.tail.value).toBe(3);
-//   });
+    // Reverse linked list back to initial state.
+    linkedList.reverse();
+    expect(linkedList.toString()).toBe('1,2,3');
+    expect(linkedList.head!.value).toBe(1);
+    expect(linkedList.tail!.value).toBe(3);
+  });
 });
